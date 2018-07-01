131aba0adda9cc11e956c8590ad62b4115cc1f3c
Ali Asgari <altostratous@gmail.com>
2018-07-01 22:01:50 +0430

    reimplement dpll passing simple tests and failing at the very first csp logical equalant test

---
+++ algorithm/logic.py
@@ -0,0 +1,112 @@
+from copy import copy
+
+
+def dpll(clauses):
+    model = {}
+    symbols = set()
+
+    for clause in clauses:
+        for literal in clause:
+            symbols.add(literal[0])
+
+    return dpll_with_model(clauses, symbols, model)
+
+
+def evaluate_clause_with_model(clause, model):
+    has_unassigned_variable = False
+    for literal in clause:
+        variable = literal[0]
+        if variable not in model:
+            has_unassigned_variable = True
+            continue
+        if literal[1] == model[variable]:
+            return True
+    if has_unassigned_variable:
+        return None
+    return False
+
+
+def evaluate_clauses_with_model(clauses, model):
+    undefined_clauses = []
+    has_clause_with_undefined_result = False
+    for clause in clauses:
+        clause_result = evaluate_clause_with_model(clause, model)
+        if clause_result is None:
+            has_clause_with_undefined_result = True
+            undefined_clauses.append(clause)
+            continue
+        if not clause_result:
+            return False, undefined_clauses
+    if has_clause_with_undefined_result:
+        return None, undefined_clauses
+    return True, undefined_clauses
+
+
+def find_pure_symbols(clauses, symbols):
+    pure_candidates = {}
+    for clause in clauses:
+        for literal in clause:
+            if literal[0] in pure_candidates:
+                if pure_candidates[literal[0]] == literal[1]:
+                    continue
+                else:
+                    del pure_candidates[literal[0]]
+            else:
+                pure_candidates[literal[0]] = literal[1]
+    result = {}
+    for symbol in pure_candidates.keys():
+        if symbol in symbols:
+            result[symbol] = pure_candidates[symbol]
+
+    if len(result) > 0:
+        return result
+    else:
+        return None
+
+
+def find_unit_clause(clauses):
+    for clause in clauses:
+        if len(clause) == 1:
+            return clause
+    return None
+
+
+def dpll_with_model(clauses, symbols, model):
+    model_result, undefined_clauses = evaluate_clauses_with_model(clauses, model)
+
+    if model_result is not None:
+        if model_result:
+            return model
+        else:
+            return None
+
+    pure_symbols = find_pure_symbols(undefined_clauses, symbols)
+
+    new_model = copy(model)
+    new_symbols = copy(symbols)
+
+    if pure_symbols is not None:
+        pure_symbol = next(iter(pure_symbols))
+        new_model[pure_symbol] = pure_symbols[pure_symbol]
+        new_symbols.remove(pure_symbol)
+        return dpll_with_model(undefined_clauses, new_symbols, new_model)
+
+    unit_clause = find_unit_clause(undefined_clauses)
+
+    if unit_clause is not None:
+        new_model[unit_clause[0][0]] = unit_clause[0][1]
+        new_symbols.remove(unit_clause[0][0])
+        return dpll_with_model(undefined_clauses, new_symbols, new_model)
+
+    first_symbol = next(iter(symbols))
+    new_symbols.remove(first_symbol)
+
+    new_model[first_symbol] = True
+    result_with_first_symbol_as_true = dpll_with_model(undefined_clauses, new_symbols, new_model)
+
+    if result_with_first_symbol_as_true is not None:
+        return result_with_first_symbol_as_true
+
+    new_model[first_symbol] = False
+    result_with_first_symbol_as_false = dpll_with_model(undefined_clauses, new_symbols, new_model)
+    return result_with_first_symbol_as_false
--- test/TestBlockLogicProblem.py
+++ test/TestBlockLogicProblem.py
@@ -1,8 +1,10 @@
 import unittest
+from unittest import skip
 
 from shapely.geometry import Polygon
 
 from algorithm.csp import dfs_with_ac3
+from algorithm.logic import dpll
 from model import BlockCSPProblem, Block
 
 
@@ -38,7 +40,7 @@ class TestBlockLogicProblem(unittest.TestCase):
         original_problem = BlockCSPProblem([
             Block(Polygon([(0, 0), (1, 0), (1, 1), (0, 1)]), 1, domain),
             Block(Polygon([(0, 0), (3, 0), (3, 3), (1, 3), (1, 2), (0, 2)]), 2, domain),
-            ], Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        ], Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
         )
 
         logic_problem = original_problem.get_propositional_logic_cnf()
@@ -50,3 +52,87 @@ class TestBlockLogicProblem(unittest.TestCase):
         self.assertTrue(TestBlockLogicProblem.does_solution_satisfy_logic_problem(solved_problem, logic_problem))
 
         self.assertFalse(TestBlockLogicProblem.does_solution_satisfy_logic_problem(original_problem, logic_problem))
+
+    def test_dpll_simple(self):
+        clauses = [
+            [('a', True)]
+        ]
+
+        result_model = dpll(clauses)
+
+        self.assertIsNotNone(result_model)
+        self.assertTrue(result_model['a'])
+
+        variable = 'a'
+        clauses = [
+            [(variable, True)],
+            [(variable, False)]
+        ]
+
+        result_model = dpll(clauses)
+
+        self.assertIsNone(result_model)
+
+    def test_dpll_complex(self):
+        first_variable = 'a'
+        second_variable = 'b'
+        third_variable = 'c'
+
+        clauses = [
+            [(first_variable, True), (second_variable, False), (third_variable, False)],
+            [(first_variable, False), (second_variable, True)],
+            [(third_variable, True)]
+        ]
+
+        result_model = dpll(clauses)
+
+        self.assertIsNotNone(result_model)
+        self.assertTrue(result_model[third_variable])
+        self.assertEqual(result_model[first_variable], result_model[second_variable])
+
+        clauses = [
+            [(first_variable, True), (second_variable, True), (third_variable, False)],
+            [(first_variable, False), (second_variable, False)],
+            [(third_variable, True)]
+        ]
+
+        result_model = dpll(clauses)
+
+        self.assertIsNotNone(result_model)
+        self.assertTrue(result_model[third_variable])
+        self.assertNotEqual(result_model[first_variable], result_model[second_variable])
+
+        clauses = [
+            [(first_variable, True), (second_variable, True), (third_variable, False)],
+            [(first_variable, False), (second_variable, False)],
+            [(third_variable, True)],
+            [(first_variable, True), (second_variable, False)],
+            [(first_variable, False), (second_variable, True)],
+        ]
+
+        result_model = dpll(clauses)
+
+        self.assertIsNone(result_model)
+
+    @skip
+    def test_simple_block_logic_problem(self):
+
+        domain = []
+        for x in range(3):
+            for y in range(3):
+                for rotation in range(0, 360, 90):
+                    domain.append(
+                        Block.Value(x, y, rotation)
+                    )
+
+        original_problem = BlockCSPProblem([
+            Block(Polygon([(0, 0), (1, 0), (1, 1), (0, 1)]), 1, domain),
+            Block(Polygon([(0, 0), (3, 0), (3, 3), (1, 3), (1, 2), (0, 2)]), 2, domain),
+        ], Polygon([(0, 0), (3, 0), (3, 3), (0, 3)])
+        )
+
+        logic_problem = original_problem.get_propositional_logic_cnf()
+
+        solved_problem = dpll(logic_problem)
+
+        self.assertIsNotNone(solved_problem)
