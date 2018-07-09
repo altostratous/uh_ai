037ff9346d855874cb7bfeba2fde0a398f04a111
parham <parham.hessabi@gmail.com>
2018-07-10 04:00:04 +0430

    improve PrioritySet performance

--- model/PrioritySet.py
+++ model/PrioritySet.py
@@ -3,26 +3,23 @@ from collections import Counter
 
 class PrioritySet:
     def __init__(self, input_list=None):
-        self.set = []
-        self.counts = []  # parallel list for number of repetition of objects in set
+        self.set = Counter()
         if input_list is not None:
             self.initiate_from_given_list(input_list)
 
     def add(self, x):
-        if self.set.__contains__(x):
-            self.counts[self.set.index(x)] += 1
-            return
-        self.set.append(x)
-        self.counts.append(1)
+        self.set[x] += 1
 
     def pop_most_frequent(self):
-        out = self.set.pop(self.counts.index(max(self.counts)))
-        self.counts.pop(self.counts.index(max(self.counts)))
+        while True:
+            try:
+                out = self.set.most_common(1)[0][0]
+                break
+            except IndexError:
+                raise Exception("Pop From Empty List")
+        self.set.pop(out)
         return out
 
     def initiate_from_given_list(self, input_list):
-        counter_list = Counter(input_list)
-        for x in counter_list:
-            self.set.append(x)
-            self.counts.append(counter_list[x])
+        self.set = Counter(input_list)
 
