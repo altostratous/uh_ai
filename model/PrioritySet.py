3a51ed1330510503e55215605b590c1cac3e8360
parham <parham.hessabi@gmail.com>
2018-07-10 06:00:23 +0430

    improve PrioritySet (now is inherited from Counter)

--- model/PrioritySet.py
+++ model/PrioritySet.py
@@ -1,25 +1,23 @@
 from collections import Counter
 
 
-class PrioritySet:
+class PrioritySet(Counter):
+    @classmethod
+    def fromkeys(cls, iterable, v=None):
+        pass
+
     def __init__(self, input_list=None):
-        self.set = Counter()
-        if input_list is not None:
-            self.initiate_from_given_list(input_list)
+        super().__init__(input_list)
 
     def add(self, x):
-        self.set[x] += 1
+        self[x] += 1
 
     def pop_most_frequent(self):
         while True:
             try:
-                out = self.set.most_common(1)[0][0]
+                out = self.most_common(1)[0][0]
                 break
             except IndexError:
                 raise Exception("Pop From Empty List")
-        self.set.pop(out)
+        self.pop(out)
         return out
-
-    def initiate_from_given_list(self, input_list):
-        self.set = Counter(input_list)
-
